8271a2aebee1de7d9a0691d295927b5d6cccf33f
wakennedy <alexkenned@gmail.com>
2020-05-04 10:13:59 -0700

    initial w/o server

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const div=document.querySelector('#showlist')
 
 //ADD SHOW FORM
 
-  const addBtn = document.querySelector("#new-toy-btn");
-  const Container = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    addform = !addform;
-    if (addform) {
-      Container.style.display = "block";
-    } else {
-      Container.style.display = "none";
-    }
-  })
+const addBtn = document.querySelector("#new-toy-btn");
+const Container = document.querySelector(".container");
+addBtn.addEventListener("click", () => {
+  addform = !addform;
+  if (addform) {
+    Container.style.display = "block";
+  } else {
+    Container.style.display = "none";
+  }
+})
 
 const form=document.getElementsByClassName("add-show")[0]
 
@@ -47,34 +47,34 @@ function makeCard(event, title){
     </h2> <img src=${title.show.image.medium} class='Toy-avatar' />`
     div.appendChild(TvDiv)
     TvDiv.addEventListener('click', (event)=> showEpisodes(event, title))
-  }
+}
 
 //ADD SHOW AND USERSHOW TO DB
-   function MakeShowDB(object) {
-      let data ={ "user_id": 1,
-                  "api_id": object.show.id,
-                  "title": object.show.name
-                  }
-      fetch('http://localhost:3000/user_shows', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    }
+function MakeShowDB(object) {
+  let data ={ "user_id": 1,
+              "api_id": object.show.id,
+              "title": object.show.name
+              }
+  fetch('http://localhost:3000/user_shows', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
 
-    //SHOW EPISODES WHEN CLICKED ON
-    function showEpisodes(event, object){
-      fetch(`http://localhost:3000/episodes/${object.show.id}`)
-    .then(resp => resp.json())
-    .then(resp=> console.log(resp))
+//SHOW EPISODES WHEN CLICKED ON
+function showEpisodes(event, object){
+  fetch(`http://localhost:3000/episodes/${object.show.id}`)
+.then(resp => resp.json())
+.then(resp=> console.log(resp))
 
-    }
+}
