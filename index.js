82a9f3a3153781d57e53ce9574b4dc622177315d
github username <github email address>
2020-05-05 09:24:43 -0700

    add and delete fixed

--- index.js
+++ index.js
@@ -13,15 +13,9 @@ function handleSearch(e) {
   e.preventDefault()
   fetch(`http://api.tvmaze.com/search/shows?q=${e.target.title.value}`)
   .then(resp => resp.json())
-  .then(resp=> showTitles(resp))
-}
-
-function showTitles(titles){
-  parent.innerText = ""
-  titles.forEach( title =>{
-      makeCard(title)
-  }
-  )
+  .then(resp=> {parent.innerText = ""
+    resp.forEach( title =>makeCard(title))
+})
 }
 
 //--------TO DO------------
@@ -29,41 +23,41 @@ function showTitles(titles){
 //FILL IN SHOW DATA
 //CONFIRM POPULATION OF CSS ELEMENTS
 function makeCard(title) {
-let card = document.createElement('div')
-card.className = "col-md-4 card-tvshow"
-card.innerHTML =
-  `<div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-12 cardimage">
-        <img src="${title.show.image.original}" alt="">
-      </div>
-      <div class="container information-box">
-        <div class="row">
-      
+  let card = document.createElement('div')
+  card.className = "col-md-4 card-tvshow"
+  let div1=document.createElement('div')
+  div1.className="container-fluid"
+  let div2=document.createElement('div')
+  div2.className="row"
+  div2.innerHTML =`
+        <div class="col-sm-12 cardimage">
+          <img src="${title.show.image.original}" alt="">
+        </div>`
+  let div3=document.createElement('div')
+  div3.className="container information-box"
+  let div4=document.createElement('div')
+  div4.className="row"
+  div4.innerHTML =`
       <div class="col-sm-6 information-left">
         <h3>${title.show.name}</h3>
         <h6>${title.show.network.name} - ${title.show.premiered}</h6>
-        <img src="img/seo-and-web.png" alt="">
-      </div>
-      <div class="col-sm-6 information-right">
-        <div class="follow">
-          <a href="#">
-            <img src="img/plus.png" alt="">
-          </a>
-        </div>
-      
-      <div class="information-button">
-        <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-          <img src="img/Information.png" alt="">
-        </a>  
-      </div>
-        
-      </div>
-    </div>
-  </div>`
+      </div>`
 
+  let follow=document.createElement('div')
+  follow.className="col-sm-6 information-right"
+  follow.innerHTML =`
+        <div class="">
+          <div class="follow" style= "cursor: pointer">
+              <img src="img/plus.png" alt="">
+        </div>`
+
+  div4.append(follow)
+  div3.appendChild(div4)
+  div2.appendChild(div3)
+  div1.appendChild(div2)
+  card.appendChild(div1)
   parent.appendChild(card)
-  card.addEventListener('click', event => handleFollow(event, title))
+  follow.addEventListener('click', event => handleFollow(event, title))
 }
 
 //TO DO
@@ -96,7 +90,8 @@ function showUserShows() {
   parent.innerText = ""
   fetch('http://localhost:3000/user_shows/1')
   .then(resp=> resp.json())
-  .then(resp=>resp.forEach(usershow => getAPIshow(usershow)))
+  .then(resp=>{
+    resp.forEach(usershow => getAPIshow(usershow))})
 }
 
 function getAPIshow(usershow){
@@ -107,46 +102,75 @@ function getAPIshow(usershow){
 }
 
 function makeusercards(title, usershow){
-  console.log(title)
   let card = document.createElement('div')
-card.className = "col-md-4 card-tvshow"
-card.innerHTML =
-  `<div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-12 cardimage">
-        <img src="${title.image.original}" alt="">
-      </div>
-      <div class="container information-box">
-        <div class="row">
-      
+  card.className = "col-md-4 card-tvshow"
+  let div1=document.createElement('div')
+  div1.className="container-fluid"
+  let div2=document.createElement('div')
+  div2.className="row"
+  div2.innerHTML =`
+        <div class="col-sm-12 cardimage">
+          <img src="${title.image.original}" alt="">
+        </div>`
+  let div3=document.createElement('div')
+  div3.className="container information-box"
+  let div4=document.createElement('div')
+  div4.className="row"
+  div4.innerHTML =`
       <div class="col-sm-6 information-left">
         <h3>${title.name}</h3>
         <h6>${title.network.name} - ${title.premiered}</h6>
         <img src="img/seo-and-web.png" alt="">
-      </div>
-      <div class="col-sm-6 information-right">
-      <div class="follow">
-      <a href="#">
-        <span class="remove"> Remove </span>
-      </a>
-    </div>
-      
+      </div>`
+
+  let follow=document.createElement('div')
+  follow.className="col-sm-6 information-right"
+  follow.innerHTML =`
+        <div class="">
+          <div class="follow" style= "cursor: pointer">
+              X
+        </div>`
+  let info=document.createElement('div')
+  info.className="col-sm-6 information-right"
+  info.innerHTML =`     
       <div class="information-button">
         <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
           <img src="img/Information.png" alt="">
         </a>  
-      </div>
-        
-      </div>
-    </div>
-  </div>`
-  card.addEventListener('click', event => handleDelete(event, usershow))
+      </div>`
+
+  div4.append(follow,info)
+  div3.appendChild(div4)
+  div2.appendChild(div3)
+  div1.appendChild(div2)
+  card.appendChild(div1)
   parent.appendChild(card)
+  info.addEventListener('click',event => showInfo(title, usershow))
+  follow.addEventListener('click', event => handleDelete(usershow))
 }
 
-function handleDelete(event, usershow){
+function handleDelete(usershow){
       fetch(`http://localhost:3000/user_shows/${usershow.id}`, {
         method: 'DELETE',
       })
       .then(showUserShows()) 
+}
+
+function showInfo(title, usershow){
+  parent.innerText = ""
+  makeusercards(title)
+  fetch(`http://localhost:3000/episodes/${usershow.show_id}`)
+  .then(resp => resp.json())
+  .then(resp=> addEpisodes(resp))
+
+}
+
+function addEpisodes(episodes) {
+let div= document.getElementsByClassName("col-sm-12")[1]
+
+episodes.forEach(ep => {
+  let li =document.createElement('li')
+  li.innerText=`name: ${ep.name}- season: ${ep.season}`
+  div.appendChild(li)
+})
 }
